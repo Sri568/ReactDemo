6593b96b4bd9f34485482507b1aaeb3f0242d667
srilakshmi_mudumba <sreelakshmi.mudumba@gmail.com>
2021-09-10 19:36:17 +0530

    app done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './img/group.png';
+ import './App.css';
+ import React,{Component} from 'react';
+ import Profile from './components/profile';
+ import WhatsAppDetail from './components/whatsapp';
+ import WhatsAppBenifits from './components/whatsappbenifts';
+ import Congrats from './components/congratulation';
+ import LoanBnfHd from './components/loanofrbenfhd';
+ import LoanAcceptance from './components/loanacceptance';
+ import {BrowserRouter as Router,Route,Link,Switch,useHistory} from 'react-router-dom';
+ import Record from './components/record';
+ import ThankYou from './components/thankyou';
 
-function App() {
+ function Content() {
+  let history = useHistory();
+
+  function handleClick() {
+    history.push("/record");
+  }
+  function thankyouClick() {
+    history.push("/thankyou");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      
+    {/* <img src= {Banner} alt="Aditya group banner" align={"left"}/><br/> */}
+
+ 
+    <p class="img-bg" >Aditya Birla Group</p> 
+    <div class="body">
+    <Profile title="Lakshmi Kantha"></Profile>
+    <WhatsAppDetail/>
+    <WhatsAppBenifits/>
+    <Congrats/>
+    <LoanBnfHd/>
+    <LoanAcceptance routeChange={handleClick} thankyouClick={thankyouClick}/>
     </div>
+ 
+</div>
   );
+}
+class App extends Component{
+
+render(){  return (
+  <Router>
+  <Route path="/record" exact component={Record} />
+  <Route path="/thankyou" exact component={ThankYou} />
+  <Route path="/" exact component={Content} />
+</Router>
+  );
+  }
 }
 
 export default App;
